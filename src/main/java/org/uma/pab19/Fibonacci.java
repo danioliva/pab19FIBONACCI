81bc6e04327f23247cfec2a521c92264d2b67172
danioliva <daniolivaglez@uma.es>
2019-04-30 13:55:51 +0200

    FibonacciRosalindIntento1

--- src/main/java/org/uma/pab19/Fibonacci.java
+++ src/main/java/org/uma/pab19/Fibonacci.java
@@ -2,16 +2,18 @@ package org.uma.pab19;
 
 public class Fibonacci {
   private int result;
-  private int partial1 = 0;
-  private int partial2 = 1;
+  private int partial1 = 1;
+  private int partial2;
   private int temporal;
 
-  public int compute(int number) {
+  public int compute(int month, int pair) {
 
-    if (number == 0 || number == 1) {
-      result = 0;
+    if (month == 1 || month == 2) {
+      result = 1;
     } else {
-      for (int i = 2; i <= number; i++) {
+      partial2 = pair;
+      for (int i = 2; i < month; i++) {
+
         result = partial1 + partial2;
         temporal = partial2;
         partial2 = temporal + partial1;
--- src/test/java/org/uma/pab19/FibonacciTest.java
+++ src/test/java/org/uma/pab19/FibonacciTest.java
@@ -21,77 +21,12 @@ public class FibonacciTest {
 
 
 
-
   @Test
-  public void shouldFibonacciOfZeroReturnZero() {
-    int expectedResult = 0;
+    public void shouldFibonacciMonth5Pairs3Return19(){
+      int expectedResult=19;
 
-    int obtainedResult = fibonacci.compute(0);
+      int obtainedResult=fibonacci.compute(5,3);
 
-    assertEquals(expectedResult, obtainedResult);
+      assertEquals(expectedResult,obtainedResult);
   }
-
-  @Test
-  public void shouldFibonacciOfOneReturnZero() {
-    int expectedResult = 0;
-
-    int obtainedResult = fibonacci.compute(1);
-
-    assertEquals(expectedResult, obtainedResult);
-  }
-
-    @Test
-    public void shouldFibonacciOfTwoReturnOne() {
-        int expectedResult = 1;
-
-        int obtainedResult = fibonacci.compute(2);
-
-        assertEquals(expectedResult, obtainedResult);
-    }
-
-    @Test
-    public void shouldFibonacciOfThreeReturnTwo() {
-        int expectedResult = 2;
-
-        int obtainedResult = fibonacci.compute(3);
-
-        assertEquals(expectedResult, obtainedResult);
-    }
-
-
-    @Test
-    public void shouldFibonacciOf4Return3() {
-        int expectedResult = 3;
-
-        int obtainedResult = fibonacci.compute(4);
-
-        assertEquals(expectedResult, obtainedResult);
-    }
-
-    @Test
-    public void shouldFibonacciOf5Return5() {
-        int expectedResult = 5;
-
-        int obtainedResult = fibonacci.compute(5);
-
-        assertEquals(expectedResult, obtainedResult);
-    }
-
-    @Test
-    public void shouldFibonacciOf6Return8() {
-        int expectedResult = 1;
-
-        int obtainedResult = fibonacci.compute(2);
-
-        assertEquals(expectedResult, obtainedResult);
-    }
-
-    @Test
-    public void shouldFibonacciOf10Return55() {
-        int expectedResult = 55;
-
-        int obtainedResult = fibonacci.compute(10);
-
-        assertEquals(expectedResult, obtainedResult);
-    }
 }
